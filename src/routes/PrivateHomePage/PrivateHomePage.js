19ae7f09a24bdd1deaa2b3725e5367f5fda942bd
Daniel Sottile <danielsottile@gmail.com>
2020-07-01 12:13:07 -0400

    setup of entire app

---
+++ src/routes/PrivateHomePage/PrivateHomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PrivateHomePage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PrivateHomePage
