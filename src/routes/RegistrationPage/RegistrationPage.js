2e5b53184d28799fc81cef368e740b5b2a399bc3
Daniel Sottile <danielsottile@gmail.com>
2020-07-01 16:29:25 -0400

    moved forms into routes

--- src/routes/RegistrationPage/RegistrationPage.js
+++ src/routes/RegistrationPage/RegistrationPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import RegistrationForm from '../../components/RegistrationForm/registration-form'
 
 export class RegistrationPage extends Component {
   render() {
     return (
       <div>
-        
+        <RegistrationForm/>
       </div>
     )
   }
