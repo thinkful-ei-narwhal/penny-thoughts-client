561038d265f0dda9b3de7fe75fea47fb350e85d1
Logan Stribling <logan.stribling@gmail.com>
2020-07-06 13:09:56 -0600

    working on user messages services

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -2,55 +2,32 @@ import config from '../config';
 import TokenService from './token-service';
 
 const messageService = {
-    getTenRandom(){
-        return fetch(`${config.API_ENDPOINT}/messages`,{
-            method: 'GET',
-            headers:{
-                'content-type': 'application/json',
-                'Authorization': `Bearer ${TokenService.getAuthToken()}`
-            }
-        })
-        .then(res => {
-          if (!res.ok) return res.json().then(e => Promise.reject(e))
-          return res.json()
-        })
-    },
-    
-    getOneRandom(){
-        return fetch(`${config.API_ENDPOINT}/messages/single`,{
-            method: 'GET',
-            headers:{
-                'content-type': 'application/json',
-                'Authorization': `Bearer ${TokenService.getAuthToken()}`
-            }
-        })
-        .then(res => {
-            if (!res.ok) {
-              error = { code: res.status};
-            }
-            return res.json();
-          })
-          .then(data => {
-            if (error) {
-              error.message = data.message;
-              return Promise.reject(error);
-            }
-            return data
-          })
-    },
-
-    getUserMessages() {
-      let error;
-      return fetch(`${config.API_ENDPOINT}/messages/userData`, {
-        method: 'GET',
-        headers: {
-          'content-type': 'application/json',
-          'Authorization': `Bearer ${TokenService.getAuthToken()}`
-        }
+  getTenRandom() {
+    return fetch(`${config.API_ENDPOINT}/messages`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) return res.json().then(e => Promise.reject(e))
+        return res.json()
       })
+  },
+
+  getOneRandom() {
+    let error;
+    return fetch(`${config.API_ENDPOINT}/messages/single`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
       .then(res => {
         if (!res.ok) {
-          error = { code: res.status};
+          error = { code: res.status };
         }
         return res.json();
       })
@@ -61,22 +38,46 @@ const messageService = {
         }
         return data
       })
-    },
+  },
 
-    deleteUserMessage(id) {
-      console.log(id)
-      let error;
-      return fetch(`${config.API_ENDPOINT}/messages/userData`, {
-        method: 'DELETE',
-        body: JSON.stringify({id}),
-        headers: {
-          'content-type': 'application/json',
-          'Authorization': `Bearer ${TokenService.getAuthToken()}`
-        }
-      })
+  getUserMessages() {
+    let error;
+    return fetch(`${config.API_ENDPOINT}/messages/userData`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
       .then(res => {
         if (!res.ok) {
-          error = { code: res.status};
+          error = { code: res.status };
+        }
+        return res.json();
+      })
+      .then(data => {
+        if (error) {
+          error.message = data.message;
+          return Promise.reject(error);
+        }
+        return data
+      })
+  },
+
+  deleteUserMessage(id) {
+    console.log(id)
+    let error;
+    return fetch(`${config.API_ENDPOINT}/messages/userData`, {
+      method: 'DELETE',
+      body: JSON.stringify({ id }),
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          error = { code: res.status };
         }
       })
       .then(data => {
@@ -87,7 +88,7 @@ const messageService = {
         }
         return data
       })
-    },
+  },
 }
 
 export default messageService;
