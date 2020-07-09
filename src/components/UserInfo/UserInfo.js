be95e504bfac28b77b1834d4269d8ecb61a0cf71
Cesar Morales <cesarlenin2@live.com>
2020-07-09 09:08:14 -0700

    handle sumit gets call and correct values are inputed

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import UserContext from '../../contexts/UserContext'
-import jwtDecode from 'jwt-decode';
-import TokenService from '../../services/token-service'
 import UserService from '../../services/userService'
 
 export class UserInfo extends Component {
@@ -15,24 +13,34 @@ export class UserInfo extends Component {
           })
           .catch(err => this.context.setError(err))
       }
+
+      handleSubmit = (fullName, email) => {
+        console.log(fullName)
+        console.log(email)
+      }
  
     render() {
         return (
-            <form>
+            <form
+            onSubmit = {(ev) => {
+                ev.preventDefault();
+                this.handleSubmit(ev.target.full_name.value,ev.target.email.value);
+              }
+            }>
                 <label htmlFor = 'full_name' > Full Name: </label> 
                 <input type = 'text'
                  name = 'full_name'
                  id = 'full_name'
+                //  ref={`${this.props.id}`}
                  defaultValue = { this.context.userData.full_name}
-                //onChange = {(e) => this.updateName(e.target.value)}
           />       
           <label htmlFor = 'email' > email: </label> 
                 <input type = 'email'
                  name = 'email'
                  id = 'email'
                 defaultValue = { this.context.userData.email }
-                //onChange = {(e) => this.updateName(e.target.value)}
           />
+          <button type = 'submit' value = 'Submit'>Submit</button>
             </form>
         )
     }
