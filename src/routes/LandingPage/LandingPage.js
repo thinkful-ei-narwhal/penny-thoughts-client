7b881dcbf2e02004af2b1cfb8fc91bf6390f6beb
Andrew Durrant <andrew.durrant75@gmail.com>
2020-07-10 12:32:15 -0600

    css fix on landing page/login-form concerning spacing

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -9,7 +9,7 @@ export class LandingPage extends Component {
       <div className="landing-page">
         <div className="landing-desc">
           <h2 className="landing-subheader">About Penny Thoughts:</h2>
-          <p>Penny Thoughts is an app that allows users to share and view positive, motivational statements. With today's quarantine keeping people at home, Penny Thoughts helps bring positivity to users by showing them positive statements. Sign up and begin reaping the benefits of positivity.</p> 
+          <p className="landing-description">Penny Thoughts is an app that allows users to share and view positive, motivational statements. With today's quarantine keeping people at home, Penny Thoughts helps bring positivity to users by showing them positive statements. Sign up and begin reaping the benefits of positivity.</p> 
           <Link className="get-inspired" to='/browse'>Get Inspired Here!</Link>
           <h2>Or...</h2>
         </div>
--- src/routes/UserSettingsPage/UserSettingsPage.js
+++ src/routes/UserSettingsPage/UserSettingsPage.js
@@ -4,7 +4,7 @@ import UserInfo from '../../components/UserInfo/UserInfo'
 export default class UserSettingsPage extends Component {
   render() {
     return (
-      <div>
+      <div className="settings-container">
         <h2 className="settings-header">Settings</h2>
         <UserMessages/>
         <UserInfo/>
