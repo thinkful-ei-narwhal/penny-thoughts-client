b1543bf8fdbc1dd8229ed0ae0924ee1f4c0ea710
Andrew Durrant <andrew.durrant75@gmail.com>
2020-07-13 09:33:35 -0600

    add mobile responsive css to LandingPage

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import LoginForm from '../../components/LoginForm/login-form'
 import {Link} from 'react-router-dom';
+import './LandingPage.css';
+
 
 export class LandingPage extends Component {
   render() {
@@ -8,7 +10,7 @@ export class LandingPage extends Component {
       <div className="landing-page">
         <div className="landing-desc">
           <h2 className="landing-subheader">About Penny Thoughts:</h2>
-          <p>Penny Thoughts is an app that allows users to share and view positive, motivational statements. With today's quarantine keeping people at home, Penny Thoughts helps bring positivity to users by showing them positive statements. Sign up and begin reaping the benefits of positivity.</p> 
+          <p className="landing-description">Penny Thoughts is an app that allows users to share and view positive, motivational statements. With today's quarantine keeping people at home, Penny Thoughts helps bring positivity to users by showing them positive statements. Sign up and begin reaping the benefits of positivity.</p> 
           <Link className="get-inspired" to='/browse'>Get Inspired Here!</Link>
           <h2 className="landing-sub-subheader">Or...</h2>
         </div>
