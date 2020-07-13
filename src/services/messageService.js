b06b7253d03882b1755a3b5afdc6c687f44d128c
Kevin Jiang <kjiang95@gmail.com>
2020-07-13 09:17:06 -0500

    working on pagination

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -45,7 +45,7 @@ const messageService = {
   },
 
   getUserMessagePageCount() {
-    return fetch(`${config.API_ENDPOINT}/messages/userData/pages`, {
+    return fetch(`${config.API_ENDPOINT}/messages/userData/page`, {
       method: 'GET',
       headers: {
         'content-type': 'application/json',
